0df28d6a5da88de3ece8f1539f71cf783c3f1584
itsquelc <raquelnunescirino@gmail.com>
2024-10-24 11:32:42 -0300

    .........

---
+++ GMovies/screens/HomeScreen.js
@@ -0,0 +1,36 @@
+import { View, Text, Platform, TouchableOpacity, ScrollView } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Bars3BottomLeftIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline'
+import { styles } from '../theme'
+
+const ios = Platform.OS == 'ios';
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 bg-neutral-800">
+      <SafeAreaView className={ios ? '-mb-2' : 'mb-3'}>
+        <StatusBar style='light'/>
+        <View className="flex-row justify-between items-center mx-4 mt-2">
+            <TouchableOpacity>
+                <Bars3BottomLeftIcon size={30} strokeWidth={2} color='white'/>
+            </TouchableOpacity>
+            <Text className='text-white text-3xl font-bold'>
+              <Text style={styles.text}> Quello </Text> Movies
+                </Text>
+            <TouchableOpacity>
+                <MagnifyingGlassIcon size={30} strokeWidth={2} color='white' />
+            </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+
+      <ScrollView
+      showsVerticalScrollIndicator={false}
+      contentContainerStyles={{ paddingBottom: 10 }}
+      >
+
+      </ScrollView>
+    </View>
+  )
+}
